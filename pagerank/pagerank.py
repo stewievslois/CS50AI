3704b877cc8475e7363f5c5edfb6a4df9350d034
stewievslois <newbold.adam@googlemail.com>
2024-03-04 16:46:54 +0000

    test

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -57,7 +57,27 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    # Create dictionary to store output in
+    output = dict()
+    
+    # Pages that are linked from current page
+    links = corpus.get(page)
+    # Probability for a certain page in corpus to be linked to from current page
+    if links:
+        p_link = damping_factor / len(links)
+    # Determine landing probability for every page
+    for pages in corpus:
+        # Probability to choose a random page out of all pages in the corpus
+        p = (1 - damping_factor) / len(corpus)
+        
+        # Add probability to land on a page via a link from the current page
+        if pages in links:
+            p = p + p_link
+        # Add page and landing probability to output dictionary
+        output.update({pages: p})
+    
+    # Return probability distribution over which page to visit next     
+    return output
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +89,44 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Create output dictionary with every page in corpus and initial PageRank value of 0
+    output = dict()
+    for pages in corpus:
+        output.update({pages: 0})
+    
+    # Choose a random starting page and update PageRank value
+    page = random.choice(list(corpus.keys()))  
+    output.update({page: 1/n})
+    
+    # Sample further only if n > 1
+    if n > 1:
+        # Sample PageRank n-1 times
+        for i in range(n-1):
+            # Get probability distribution for current page
+            p_dict = transition_model(corpus, page, damping_factor)
+            
+            # Create a list with every page and cumulative probability
+            p_list = []
+            sum_p = 0
+            for key in p_dict:
+                sum_p = sum_p + p_dict.get(key)
+                p_list.append([key, sum_p])
+            
+            # Take a random number between 0 and 1
+            rand = random.uniform(0,1)
+          
+            # Choose a random page based on the probability distribution
+            for i in range(len(p_list)):
+                if rand < p_list[i][1]:
+                    page = p_list[i][0]
+                    break
+            
+            # Update PageRank value for chosen page
+            PR = output.get(page) + 1/n
+            output.update({page: PR})
+    
+    # Return dictionary with pagenames and PageRank values   
+    return output
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +138,47 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Create output dictionary with every page in corpus and initial PageRank value of 1/(number of pages)
+    output = dict()
+    for pages in corpus:
+        p = 1 / len(corpus)
+        output.update({pages: p})
+        
+    # Repeat until PageRank values do not change more than 0.001
+    while True:
+        # Initiate a variable to measure PageRank value change
+        dif = 0
+        # Update PageRank value for every page p in corpus
+        for page in output:
+            # PageRank value of current page p
+            PR = output.get(page)
+            # Intialise new PageRank value for page p as with the probability to land on the page at random
+            PR_new = (1 - damping_factor) / len(corpus)
+            # Calculate PageRank contribution from other pages i
+            for otherpage in corpus:
+                # PageRank value of page i
+                PR_link = output.get(otherpage)
+                # Number of links on page i
+                NumLinks = len(corpus.get(otherpage))
+                # If page i has no links, it is treated as having a link to every page
+                if NumLinks == 0:
+                    PR_new = PR_new + damping_factor * PR_link / len(corpus)    
+                # Add Sum part of PageRank equation for every page i that links to page p
+                elif page in corpus.get(otherpage):
+                    # Add contribution of page i to the PageRank of page p, as per the PageRank formula
+                    PR_new = PR_new + damping_factor * PR_link / NumLinks
+            # Save new PageRank value in output dictionary
+            output.update({page: PR_new})
+            # Calculate the change in PageRank value and save this value if it's larger than for other pages
+            newdif = abs(PR_new - PR)
+            dif = max(dif, newdif)
+        # Break if PageRank values do not change more than 0.001
+        if dif < 0.001:
+            break
+    
+    # Return dictionary with pagenames and PageRank values   
+    return output
+
 
 
 if __name__ == "__main__":
